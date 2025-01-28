3a4e030003843fc4a1ca403e7f60527cc1b94010
GitHub <noreply@github.com>
2025-01-28 05:25:11 +0000

    :arrow_up: update deno dependencies

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-import "https://deno.land/x/dotenv@v3.2.0/load.ts";
-export * from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.14.9/rest/page-data.ts";
+import "https://deno.land/x/dotenv@v3.2.2/load.ts";
+export * from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.29.8/rest/page-data.ts";
 //import yaml parser
-export { parseAll } from "https://deno.land/std@0.163.0/encoding/yaml.ts";
-export { assertString } from "https://deno.land/x/unknownutil@v2.0.0/assert.ts";
-export { assertArray } from "https://deno.land/x/unknownutil@v2.0.0/assert.ts";
+export { parseAll } from "https://deno.land/std@0.224.0/encoding/yaml.ts";
+export { assertString } from "https://deno.land/x/unknownutil@v3.18.1/assert.ts";
+export { assertArray } from "https://deno.land/x/unknownutil@v3.18.1/assert.ts";
